ad579e5deaaa368a5580c8426d4e5e97da95dfbb
lex_19 <lex.19.mov@gmail.com>
2019-05-27 15:36:58 +0300

    90% of work (i think so)

--- js/script.js
+++ js/script.js
@@ -8,6 +8,38 @@ $(document).ready(function () {
     arrows: false,
     appendDots: $('.my_dots_append')
   });
+  $('.testi_slick_container').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    // fade: true,
+    // cssEase: 'linear',
+    arrows: false,
+    draggable:false,
+    asNavFor: '.dots_slick_container',
+    
+  });
+  $('.dots_slick_container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: false,
+    speed: 500,
+    prevArrow: $('.my_prev_arrow'),
+    nextArrow: $('.my_next_arrow'),
+    // fade: true,
+    // cssEase: 'linear',
+    arrows: true,
+    appendArrows: $('.dots_slick_container'),
+    asNavFor: '.testi_slick_container',
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: '70px',
+    
+    
+  });
 });
 
 // poster frame click event
@@ -43,3 +75,20 @@ function videoStop($wrapper) {
   // remove youtube link, stopping the video from playing in the background
   $iframe.attr('src', '');
 }
+
+jQuery(function ($) {
+  $('a[href*="#"]').on('click.smoothscroll', function (e) {
+    var hash = this.hash,
+      _hash = hash.replace(/#/, ''),
+      theHref = $(this).attr('href').replace(/#.*/, '');
+    if (theHref && location.href.replace(/#.*/, '') != theHref) return;
+    var $target = _hash === '' ? $('body') : $(hash + ', a[name="' + _hash + '"]').first();
+    if (!$target.length) return;
+    e.preventDefault();
+    $('html, body').stop().animate({
+      scrollTop: $target.offset().top - 0
+    }, 400, 'swing', function () {
+      window.location.hash = hash;
+    });
+  });
+});
